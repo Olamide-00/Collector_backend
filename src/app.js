0041d0b4354f27efea9746f2b4927e586d2f9988
olamide <officialolamide001@gmail.com>
2026-07-10 21:15:04 +0100

    updated

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.set("trust proxy", 1);
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://collected-frontend.onrender.com/",
+  "https://collected-frontend.onrender.com",
 ];
 
 app.use(helmet());
