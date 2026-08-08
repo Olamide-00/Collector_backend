0402434ce5854e4f05127efd5690b2da081a4fcf
Olamide Oladele <officialolamide001@gmail.com>
2026-08-08 18:54:53 +0100

    delete and edit amount by admin implemented

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,12 +2,23 @@ import { Router } from 'express'
 import { restrictTo } from '../middleware/role.middleware.js'
 import { validate } from '../middleware/validate.middleware.js'
 import { ROLES } from '../constants/roles.js'
-import { addPayment, getPayments } from '../controllers/payment.controller.js'
-import { addPaymentSchema } from '../validations/payment.validation.js'
+import { addPayment, getPayments, editPayment } from '../controllers/payment.controller.js'
+import {
+  addPaymentSchema,
+  editPaymentSchema,
+  paymentIdParamSchema,
+} from '../validations/payment.validation.js'
 
 const router = Router({ mergeParams: true })
 
 router.get('/', getPayments)
 router.post('/', restrictTo(ROLES.ADMIN), validate(addPaymentSchema), addPayment)
+router.patch(
+  '/:paymentId',
+  restrictTo(ROLES.ADMIN),
+  validate(paymentIdParamSchema, 'params'),
+  validate(editPaymentSchema),
+  editPayment
+)
 
 export default router
